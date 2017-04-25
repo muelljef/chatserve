7991768acd9ed40cfef0644b5a965ec26e2bb9ef
muelljef <muelljef@oregonstate.edu>
2017-04-25 13:26:36 +1000

    added a loop to the communication

--- chatserve.py
+++ chatserve.py
@@ -2,16 +2,27 @@
 from socket import *
 
 serverName = 'localhost'
-serverPort = 50516
+serverPort = 50517
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print 'The server is ready to receive'
+
+serverHandle = 'ServeO'
+
 while 1:
     connectionSocket, addr = serverSocket.accept()
 
-    sentence = connectionSocket.recv(1024)
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+    while 1:
+        peerMessage = connectionSocket.recv(1024)
+        if not peerMessage:
+            break
+        print peerMessage
+
+        myMessage = raw_input(serverHandle + '>')
+        myMessage = serverHandle + '>' + myMessage
+        connectionSocket.send(myMessage)
+
+    print "\nConnection terminated, still serving for chat\n"
+
+connectionSocket.close()
