c1e218e08f96b905f6efe044a067797bb79edc0a
muelljef <muelljef@oregonstate.edu>
2017-04-29 16:52:56 +1000

    adding ability for server to quit, adding comments

--- chatserve.py
+++ chatserve.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 from socket import *
 
 serverName = 'localhost'
@@ -10,19 +9,32 @@ serverSocket.listen(1)
 
 serverHandle = 'ServeO'
 
+# listen for connection
 while 1:
     connectionSocket, addr = serverSocket.accept()
 
+    # connection made, loop for conversation
     while 1:
+        # read the client message
         peerMessage = connectionSocket.recv(1024)
+
+        # check if client closed the connection, if not print the message to screen
         if not peerMessage:
+            "client closed connection"
             break
         print peerMessage
 
+        # get the message from the server
         myMessage = raw_input(serverHandle + '>')
+
+        # check if the server wants to quit the connection
+        if myMessage == "\quit":
+            print "closing connection"
+            break;
+
+        # send the message to the client
         myMessage = serverHandle + '>' + myMessage
         connectionSocket.send(myMessage)
 
+    connectionSocket.close()
     print "\nConnection terminated, still serving for chat\n"
-
-connectionSocket.close()
