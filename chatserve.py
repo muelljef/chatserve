8446a4e380b604e18729ae6647cab4e2c1996b35
muelljef <muelljef@oregonstate.edu>
2017-05-01 14:08:41 +1000

    getting the port number from the command line

--- chatserve.py
+++ chatserve.py
@@ -2,20 +2,35 @@ from socket import *
 import signal
 import sys
 
+# Setting a SIG INT or Ctrl + C handler to close the server socket on exit
 def signal_handler(signal, frame):
     serverSocket.close()
     print('You pressed Ctrl+C')
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-serverName = 'localhost'
-serverPort = 50517
+# Get the port number from the user
+try:
+    serverPort = int(raw_input('Enter the port number to serve on: '))
+except ValueError:
+    print 'Invalid port number'
+    sys.exit(1)
+if serverPort < 1 or serverPort > 65535:
+    print 'Invalid port number'
+    sys.exit(1)
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+# Setup the server connection on the given port number
+try:
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(1)
+except:
+    print 'Failed to bind the port'
+    sys.exit(1)
+print 'Listening for connections on port ' + str(serverPort) + '\n'
 
+# hard coded server handle
 serverHandle = 'ServeO'
 
 # listen for connection
